5d4128e0e751b7d81854acdc1545d062f90a092c
KordianJanowski <kordianj1227@gmail.com>
2021-09-24 00:00:10 +0200

    Added Carousel component to About view

--- src/views/About.vue
+++ src/views/About.vue
@@ -4,18 +4,22 @@
     
     <AboutComponent :aditionalClass="'my-12 2xl:my-20 lg:mt-32 2xl:mt-40'" />
 
+    <Carousel class="mb-20" />
+
     <Footer />
   </div>
 </template>
 <script>
 import Navbar from '../components/Navbar'
 import AboutComponent from '../components/AboutComponent.vue'
+import Carousel from '../components/Carousel.vue'
 import Footer from '../components/Footer.vue'
 
 export default {
   components: {
     Navbar,
     AboutComponent,
+    Carousel,
     Footer,
   }
 }
